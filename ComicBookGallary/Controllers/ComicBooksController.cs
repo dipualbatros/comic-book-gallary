86f28878822539c927b1b046150884f54b114bc5
Dipu <Dipu@DIPU>
2017-02-17 15:16:29 +0100

    Added ComicBook Detail view.

--- ComicBookGallary/Controllers/ComicBooksController.cs
+++ ComicBookGallary/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace ComicBookGallary.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic book controller!");
+            return View();
         }
     }
 }
